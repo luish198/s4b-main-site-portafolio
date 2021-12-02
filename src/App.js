c65cc356ea94f7f3fcc29fd7a559ecc4d92351ac
luish198 <luish198@hotmail.com>
2021-12-02 10:53:18 +0100

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,115 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
-//React Fontawesome imports
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/react-fontawesome'
+
+
+
 
 
 function App() {
+
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const canvasClassName = "tsparticles-canvas-el"
+
+
   return (
-    <>
+    <> 
+      <Particles className={canvasClassName}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 900,
+            },
+            value: 30,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+            stroke:{
+              width:6,
+              color: "#f9ab00"
+            }
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }} />
       <Navbar />
+      <Header />
+
     </>
   );
 }
